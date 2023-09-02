4cc7e2325f192a8eda430a9fa14403536c9c0acc
Marcelo Santana <marcelo.ssantos127@gmail.com>
2023-09-02 19:12:20 -0300

    Separate files js API

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,25 @@
-console.log("Sucesso!");
+
+
+function convertPokemonToHtml(pokemon) {
+    return `
+    <li class="pokemon">
+        <span class="number">#001</span>
+        <span class="name">${pokemon.name}</span>
+        <div class="detail">
+            <ol class="types">
+                <li class="type">grass</li>
+                <li class="type">poison</li>
+            </ol>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="${pokemon.name}">
+        </div>
+    </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemons');
+
+pokeApi.getPokemons() 
+    .then((pokemons = []) => {    
+        pokemonList.innerHTML += pokemons.map(convertPokemonToHtml).join('');
+    })
+
